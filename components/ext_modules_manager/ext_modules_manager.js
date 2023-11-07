3cb35d6df094ab0a3277dcb441a0ab16ce550e9c
Andrea Guizzaro <guizzaro.1812828@studenti.uniroma1.it>
2023-11-07 21:39:43 +0100

    external modules manager implemented

---
+++ components/ext_modules_manager/ext_modules_manager.js
@@ -0,0 +1,30 @@
+const Wss_manager = require("../../connections/wss_manager");
+const Link_manager = require("../../connections/link_manager");
+
+class Ext_Module_Manager
+{   
+    constructor()
+    {
+        this.link_manager = new Link_manager("MODULES_MGMT", "modules_queue");
+        this.wss_manager = new Wss_manager();
+        this.link_manager.start();
+        this.wss_manager.start();
+
+        this.wss_manager.on("msg", this.from_client.bind(this));
+        this.link_manager.on("msg", this.send_client.bind(this));
+    }
+
+    from_client(msg) 
+    {
+        return this.link_manager.to_core("core_queue", msg);
+    }
+
+    send_client(msg)
+    {   
+        return this.wss_manager.send_response(msg);
+    }
+
+}
+
+
+module.exports = Ext_Module_Manager
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var  Client_Manager = require("./components/clients_manager/client_manager");
 var Core = require("./components/core/core");
+const Ext_Module_Manager = require("./components/ext_modules_manager/ext_modules_manager");
 const Wether_module = require("./modules/wether_modules");
 
-var client_manager = new Client_Manager()
 var core = new Core()
+var client_manager = new Client_Manager()
+// var ext_module_manager = new Ext_Module_Manager();
 var wether = new Wether_module();
 
 wether.start();
