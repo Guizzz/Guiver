6f1fdd622169830709da03d37311ca7dd148f32c
Andrea Guizzaro <guizzaro.1812828@studenti.uniroma1.it>
2023-11-17 22:47:17 +0100

    init rainbow

--- modules/led_module.js
+++ modules/led_module.js
@@ -10,7 +10,8 @@ class Led_module extends Module
             "led_manual": this.led_manual_mgmt.bind(this)
         });
         this._init_led();
-        this._set_led()
+        this._set_led();
+        this.rainbowRunning = false;
     }
 
     _init_led()
@@ -52,6 +53,96 @@ class Led_module extends Module
         };
 
         this.link_manager.to_core("core_queue", JSON.stringify(request));
+    }
+
+    setRainbow(request)
+    {
+        var time=request.payload.time;
+    
+        request.type = "response";
+        if(this.rainbowRunning) 
+        {
+            request.error = "Rainbow already running";
+            this.link_manager.to_core("core_queue", JSON.stringify(request));
+            return;
+        }
+            
+        if(time==undefined)
+        {
+            request.error = "Time delay is undefined";
+            this.link_manager.to_core("core_queue", JSON.stringify(request));
+            return;
+        }
+
+        if(time<20) //to do
+        {
+            request.error = "Time frequence is too high";
+            this.link_manager.to_core("core_queue", JSON.stringify(request));
+            return;
+        }
+
+        this.time = parseInt(time,10);
+        this.rainbowRunning=true;
+
+        this.startRainbow();
+
+        return({success:1});
+    }
+
+    async _startRainbow()
+    {
+        for(; this.redValue<this.rainbowBrightness; this.redValue++)
+        {
+            this.RedLed.pwmWrite(parseInt(this.redValue,10));
+            await this.sleep(this.time);
+        }
+
+        while(this.rainbowRunning)
+        {
+            for(; this.greenValue<this.rainbowBrightness && this.rainbowRunning; this.greenValue++)
+            {
+                this.GreenLed.pwmWrite(parseInt(this.greenValue,10));
+                await this.sleep(this.time);
+            }
+            
+            for(; this.redValue>0 && this.rainbowRunning; this.redValue--)
+            {
+                this.RedLed.pwmWrite(parseInt(this.redValue,10));
+                await this.sleep(this.time);
+            }
+
+            for(; this.blueValue<this.rainbowBrightness && this.rainbowRunning; this.blueValue++)
+            {
+                this.BlueLed.pwmWrite(parseInt(this.blueValue,10));
+                await this.sleep(this.time);
+            }
+
+            for(; this.greenValue>0 && this.rainbowRunning; this.greenValue--)
+            {
+                this.GreenLed.pwmWrite(parseInt(this.greenValue,10));
+                await this.sleep(this.time);
+            }
+
+            for(; this.redValue<this.rainbowBrightness && this.rainbowRunning; this.redValue++)
+            {
+                this.RedLed.pwmWrite(parseInt(this.redValue,10));
+                await this.sleep(this.time);
+            }
+
+            for(; this.blueValue>0 && this.rainbowRunning; this.blueValue--)
+            {
+                this.BlueLed.pwmWrite(parseInt(this.blueValue,10));
+                await this.sleep(this.time);
+            }
+        }
+
+        for(var i=255; i>0 ; i--)
+        {
+            if(this.redValue>0)   this.RedLed.pwmWrite(parseInt(this.redValue--,10));
+            if(this.greenValue>0) this.GreenLed.pwmWrite(parseInt(this.greenValue--,10));
+            if(this.blueValue>0)  this.BlueLed.pwmWrite(parseInt(this.blueValue--,10));
+            await this.sleep(parseInt(this.time/2, 10));
+        }
     }
     
 }
