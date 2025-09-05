d70b43d1f60a1e674f5ab644f5584956c7801cce
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2025-09-05 19:16:23 +0200

    relay update

--- components/modules/relay_module.js
+++ components/modules/relay_module.js
@@ -1,26 +1,55 @@
 const Module = require("./module");
-var Gpio = require('pigpio').Gpio;
+var Gpio = null;
+
+try
+{
+    Gpio = require('pigpio').Gpio;
+}
+catch
+{
+    console.log("ERORE")
+}
+
+var REALAY_PIN = 5;
 
 class Relay_module extends Module
 {
     constructor()
     {
-        super("LED_MODULE", "led_queue");
+        super("RELAY_MODULE", "relay_queue");
         this.set_handled_cmds({
-            "light_on": this.light_on.bind(this),           
+            "light_on": this.light_on.bind(this),
+            "light_off": this.light_off.bind(this),
         });
         this._init_led();
     }
     _init_pin()
     {
         this.main_light_status=0;
-        this.main_light = new Gpio(5, {mode: Gpio.OUTPUT});
+        this.main_light = new Gpio(REALAY_PIN, {mode: Gpio.OUTPUT});
         this.main_light.digitalWrite(this.main_light_status,10);
     }
 
     light_on() {
         this.main_light_status=1;
         this.main_light.digitalWrite(this.main_light_status,10);
+    }
+
+    light_off() {
+        this.main_light_status=1;
+        this.main_light.digitalWrite(this.main_light_status,10);
+    }
+
+    relay_status(request)
+    {
+        var resp = {
+            "type": "response",
+            "command": "relay_status",
+            "payload": {
+                "relay_status": this.main_light_status
+            }
+        }
+        return resp;
     }
 
 }
