f43a373a907d3427c3734cf1be564c4d8f33eafc
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2025-09-06 18:17:49 +0200

    fix blacklist

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 var fs = require('fs');
 var modules = JSON.parse(fs.readFileSync("components/all_modules.json", 'utf8'));
 
-var white_list = ["homekit_server"]
+var black_list = ["homekit_server"]
 
 for (mod in modules)
 {
-    if (!white_list.includes(mod))
+    if (black_list.includes(mod))
     {
         console.log("Ignoring: "+ mod);
         continue;
